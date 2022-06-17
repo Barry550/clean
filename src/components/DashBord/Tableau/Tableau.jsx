76765e7de90da235920a936f6c68e36632e8fc42
Oumou550 <bahsadio868@gmail.com>
2022-06-17 21:09:59 +0000

    update firsion

--- src/components/DashBord/Tableau/Tableau.jsx
+++ src/components/DashBord/Tableau/Tableau.jsx
@@ -8,7 +8,6 @@ export default function({filtering, filter}) {
   const user = state.userApi.user
   return (
     <div className="card-body">
-
     <table id="datatablesSimple">
          <EntetePied  data={filtering ? filter : user?.abonnes}/>    
 </table>
